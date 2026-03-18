d2a49ebf658b0316924d3819a7454e32af84cfe6
Ngocphuoc2206 <tranngocphuoc.2000vta@gmail.com>
2026-03-18 10:57:11 +0700

    Update structure frontend

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,9 @@
+import UserLayout from "@/components/templates/UserLayout/UserLayout";
+
+export default function AdminDashboardPage() {
+  return (
+    <UserLayout title="Admin Dashboard">
+      <div className="mx-auto max-w-7xl px-6 py-10">Admin Dashboard</div>
+    </UserLayout>
+  );
+}
---
+++ src/pages/customer/index.tsx
@@ -0,0 +1,9 @@
+import UserLayout from "@/components/templates/UserLayout/UserLayout";
+
+export default function CustomerDashboardPage() {
+  return (
+    <UserLayout title="Customer Dashboard">
+      <div className="mx-auto max-w-7xl px-6 py-10">Customer Dashboard</div>
+    </UserLayout>
+  );
+}
