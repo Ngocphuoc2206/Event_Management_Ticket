3b7bdaa7ec564fb7ec75240b9556d2603a50da5d
Hieutran <trunghieunaruto204@gmail.com>
2026-03-20 07:44:47 +0700

    Implement register API

---
+++ src/main/java/com/envenHub/backend/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.envenHub.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Tắt CSRF
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(auth -> auth
+                                //API public
+                                .requestMatchers(HttpMethod.GET, "/api/health").permitAll()
+                                .requestMatchers("/auth/register","/auth/login").permitAll()
+
+                                // API authenticated
+                                .requestMatchers("/api/users/**").authenticated()
+                                .anyRequest().authenticated()
+                )
+
+                // HTTP Basic
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/envenHub/backend/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.envenHub.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String fullName;
+    private String phone;
+    private String password;
+    private String email;
+}
