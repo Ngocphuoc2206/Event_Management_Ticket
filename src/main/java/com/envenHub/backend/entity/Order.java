d55ac1426e295d36cb7fdc1562a85753502444c7
Hieutran <trunghieunaruto204@gmail.com>
2026-04-07 07:16:26 +0700

    Implement order creation APIs

---
+++ src/main/java/com/envenHub/backend/entity/Order.java
@@ -0,0 +1,31 @@
+package com.envenHub.backend.entity;
+
+import com.envenHub.backend.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private LocalDateTime orderDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+}
---
+++ src/main/java/com/envenHub/backend/entity/TicketType.java
@@ -0,0 +1,26 @@
+package com.envenHub.backend.entity;
+
+import com.envenHub.backend.enums.TicketTypeStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class TicketType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer soldQuantity;
+    private LocalDateTime saleStart;
+    private LocalDateTime saleEnd;
+
+    @Enumerated(EnumType.STRING)
+    private TicketTypeStatus status;
+}
