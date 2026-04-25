989cded82518078ef0adb4f94ad0bb5f26649694
Ngocphuoc2206 <tranngocphuoc.2000vta@gmail.com>
2026-04-25 09:57:11 +0700

    Update

--- src/features/customer/events.service.ts
+++ src/features/customer/events.service.ts
@@ -388,10 +388,7 @@ export async function getPublicEventDetail(eventId: string) {
     ticketCount: ticketTypes.length || summary.ticketCount,
     availableTickets:
       ticketTypes.length > 0
-        ? ticketTypes.reduce(
-            (sum, ticketType) => sum + ticketType.availableQuantity,
-            0,
-          )
+        ? ticketTypes.reduce((sum, ticketType) => sum + ticketType.quantity, 0)
         : summary.availableTickets,
   } satisfies CustomerEventDetail;
 }
